842953da95132d77b70e001b4cafede6cca0611a
UIT.24730129 <24730129@ms.uit.edu.vn>
2025-04-22 09:01:56 +0700

    Initial commit: Code mẫu cho dự án Snake Game

---
+++ main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <conio.h>
+#include <ctime>
+using namespace std;
+
+// Khai bao cac thu vien co ban va sưa ham Gotoxy
+void gotoxy( int column, int line )
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle( STD_OUTPUT_HANDLE ),
+        coord
+    );
+}
+// End Khai bao cac thu vien co ban va sưa ham Gotoxy
+// Chinh sua Struct Point
+struct Point
+{
+    int x, y;
+};
+// End Chinh sua Struct Point
+ SnakeClass
+
+//Snake Class
+class Snake
+{
+// Bổ sung nội dung tại đây
+
+class CONRAN{
+main
+public:
+    Point body[100];
+    int length;
+    int direction;
+    int prevDirection;
+
+}
+//End Snake Class
+    struct Point A[100];
+    int DoDai;
+    CONRAN(){
+        DoDai = 3;
+        A[0].x = 10; A[0].y = 10;
+        A[1].x = 11; A[1].y = 10;
+        A[2].x = 12; A[2].y = 10;
+    }
+    void Ve(){
+        for (int i = 0; i < DoDai; i++){
+            gotoxy(A[i].x,A[i].y);
+            cout<<"X";
+        }
+    }
+    void DiChuyen(int Huong){
+        for (int i = DoDai-1; i>0;i--)
+            A[i] = A[i-1];
+        if (Huong==0) A[0].x = A[0].x + 1;
+        if (Huong==1) A[0].y = A[0].y + 1;
+        if (Huong==2) A[0].x = A[0].x - 1;
+        if (Huong==3) A[0].y = A[0].y - 1;
+
+    }
+};
+//DrawBoard
+void DrawBoard(int width, int height)
+{
+    for (int i = 0; i < width; ++i)
+    {
+        gotoxy(i, 0);
+        cout << "=";
+        gotoxy(i, height - 1);
+        cout << "=";
+    }
+    for (int i = 0; i < height; ++i)
+    {
+        gotoxy(0, i);
+        cout << "=";
+        gotoxy(width - 1, i);
+        cout << "=";
+    }
+}
+
+//End DrawBoad
+int main()
+{
+    CONRAN r;
+    int Huong = 0;
+    char t;
+
+    while (1){
+        if (kbhit()){
+            t = getch();
+            if (t=='a') Huong = 2;
+            if (t=='w') Huong = 3;
+            if (t=='d') Huong = 0;
+            if (t=='x') Huong = 1;
+        }
+        system("cls");
+        r.Ve();
+        r.DiChuyen(Huong);
+        Sleep(300);
+    }
+
+    return 0;
+}
+
+
+
